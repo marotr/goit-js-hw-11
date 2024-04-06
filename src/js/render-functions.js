dd225403b843d920401c2d51fb3cd0254413f9de
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-04-06 15:00:39 +0300

    almost done

---
+++ src/js/render-functions.js
@@ -0,0 +1,19 @@
+const images = {
+	method: "GET"
+};
+export function createMarkup(arr) {
+return arr.map(({ webformatURL, tags, largeImageURL, likes, views, comments, downloads }) => {
+           return ` <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}" >
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" width = "360"/>
+          <ul class="card-descr">
+           <li class="card-element"><span class="element-label">Likes:</span> ${likes}</li>
+            <li class="card-element"><span class="element-label">Views:</span> ${views}</li>
+            <li class="card-element"><span class="element-label">Comments:</span> ${comments}</li>
+            <li class="card-element"><span class="element-label">Downloads: </span>${downloads}</li></ul>
+        </a>
+      </li>`
+            
+        }).join("");
+}
+
