dfee3c12ce18006d7bd5b1f5373c1ddf1f2fa782
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-04-06 17:49:02 +0300

    css

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -10,10 +10,10 @@ return arr.map(({ webformatURL, tags, largeImageURL, likes, views, comments, dow
            alt="${tags}" 
            width = "360"/>
           <ul class="card-descr">
-           <li class="card-element"><span class="element-label">Likes:</span> ${likes}</li>
-            <li class="card-element"><span class="element-label">Views:</span> ${views}</li>
-            <li class="card-element"><span class="element-label">Comments:</span> ${comments}</li>
-            <li class="card-element"><span class="element-label">Downloads: </span>${downloads}</li></ul>
+           <li class="card-element"><span class="label">Likes</span> <p class ="value">${likes}</p></li>
+            <li class="card-element"><span class="label">Views</span> <p class ="value">${views}</p></li>
+            <li class="card-element"><span class="label">Comments</span> <p class ="value">${comments}</p></li>
+            <li class="card-element"><span class="label">Downloads</span><p class ="value">${downloads}</p></li></ul>
         </a>
       </li>`
             
